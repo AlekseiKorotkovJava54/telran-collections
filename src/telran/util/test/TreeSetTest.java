09886ea640d1d0c09b9bb6d4cd77fa914ff853a0
AlekseiKorotkov159 <158587440+AlekseiKorotkov159@users.noreply.github.com>
2024-06-18 22:37:44 +0300

    HW 29

--- src/telran/util/test/TreeSetTest.java
+++ src/telran/util/test/TreeSetTest.java
@@ -2,6 +2,9 @@ package telran.util.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,9 +22,7 @@ public class TreeSetTest extends SortedSetTest {
 	}
 	@Test
 	void displayRootChildrenTest() {
-		treeSet.setSpacesPerLevel(4);
 		treeSet.displayRootChildren();
-		System.out.println();
 	}
 	@Test
 	void treeInversionTest() {
@@ -40,6 +41,7 @@ public class TreeSetTest extends SortedSetTest {
 		treeSet.setSpacesPerLevel(4);
 		treeSet.displayTreeRotated();
 	}
+	
 	@Test
 	void widthTest() {
 		assertEquals(2, treeSet.width());
@@ -48,5 +50,47 @@ public class TreeSetTest extends SortedSetTest {
 	@Test
 	void heightTest() {
 		assertEquals(4, treeSet.height());
+	}
+	@Test
+	void sortedSequenceTreeTest() {
+		TreeSet<Integer> treeSet = new TreeSet<>();
+		int[] sortedArray = new Random().ints().distinct()
+				.limit(N_ELEMENTS).sorted().toArray();
+		transformArray(sortedArray);
+		for(int num: sortedArray) {
+			treeSet.add(num);
+		}
+		balancedTreeTest(treeSet);
+	}
+	
+	private void balancedTreeTest(TreeSet<Integer> treeSet) {
+		assertEquals(20, treeSet.height());
+		assertEquals((N_ELEMENTS + 1) / 2, treeSet.width());
+	}
+	
+	private void transformArray(int[] sortedArray) {
+		int [] sourceArray = Arrays.copyOf(sortedArray, sortedArray.length);
+		transformArray(sortedArray, sourceArray,0, 0, sortedArray.length-1);
+	}
+	
+	private void transformArray(int[] sortedArray, int[] sourceArray, int newIndex, int left, int right) {
+		if(left <= right && newIndex < sortedArray.length) {
+			int middle = (left + right) / 2;
+			sortedArray[newIndex] = sourceArray[middle];
+			transformArray(sortedArray, sourceArray, newIndex*2+1, left, middle - 1);
+			transformArray(sortedArray, sourceArray, newIndex*2+2, middle + 1, right);
+		}
+	}
+	
+	@Test
+	void balanceTreeTest() {
+		createBigRandomCollection(new Random());
+		treeSet.balance();
+		balancedTreeTest(treeSet);
+		int index = 0;
+		for(Integer num: treeSet) {
+			index++;
+		}
+		assertEquals(treeSet.size(), index);
 	}
 }
